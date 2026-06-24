4036d76cd6523bc0a8b2284a63096baba411e03a
WojciechKoziestanski <wkoziestanski@gmail.com>
2026-06-25 01:10:05 +0200

    refactor: remove non-taskplanner files from studies-release

--- src/main/java/io/github/wojciechkoziestanski/Idea.java
+++
@@ -1,4 +0,0 @@
-package io.github.wojciechkoziestanski;
-
-public class Idea {
-}
--- src/main/java/io/github/wojciechkoziestanski/core/AppModule.java
+++
@@ -1,9 +0,0 @@
-package io.github.wojciechkoziestanski.core;
-
-
-import javafx.scene.Scene;
-
-public interface AppModule {
-    String getModuleName();
-    Scene getScene();
-}
